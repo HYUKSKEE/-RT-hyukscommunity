111efd7be9e915a7c2b1fcce89d918f93746e79f
HYUKSKEE <gin280833@gmail.com>
2024-02-15 12:48:49 +0900

    add: react router dom 라이브러리 추가 및 라우팅 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/posts" element={<div>Posts page</div>} />
+      <Route path="/posts/new" element={<div>Post new page</div>} />
+      <Route path="/posts/:id" element={<div>Post detail page</div>} />
+      <Route path="/posts/edit/:id" element={<div>Post edit page</div>} />
+      <Route path="/profile" element={<div>Profile page</div>} />
+      <Route path="/profile/edit" element={<div>Profile edit page</div>} />
+      <Route path="/notifications" element={<div>Notifications page</div>} />
+      <Route path="/search" element={<div>Search page</div>} />
+      <Route path="/users/login" element={<div>Login page</div>} />
+      <Route path="/users/signup" element={<div>Signup page</div>} />
+      <Route path="/*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 }
 
